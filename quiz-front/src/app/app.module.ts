551842f0408eaf4871cc1516a9d758c6bd4f1911
Arina Topor <arinatopor695@gmail.com>
2024-08-11 19:24:32 +0500

    init frontend

---
+++ quiz-front/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { QuizCardComponent } from './shared/components/quiz-card/quiz-card.component';
+
+@NgModule({
+  declarations: [AppComponent, QuizCardComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ quiz-front/src/app/pages/main/main.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MainComponent } from './component/main.component';
+
+@NgModule({
+  declarations: [MainComponent],
+  imports: [BrowserModule],
+  providers: [],
+})
+export class MainModule {}
